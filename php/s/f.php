f34fe5822ed182cfb3d0dbd25c5c8e85650bca8a
unknown <it@thinksurfmedia.com>
2023-12-19 11:49:03 +0530

    6 commit

---
+++ php/s/f.php
@@ -0,0 +1,9 @@
+<?php
+
+$static_dir = __DIR__;
+$root = '/home/thinksur/kalyanhospital.thinksurfmedia.com/php/';
+$plugin_root = $root . 'wp-content/plugins/psn-pagespeed-ninja';
+
+define('RESSIO_STATICDIR', $static_dir);
+
+require $plugin_root . '/ress/fetch.php';
