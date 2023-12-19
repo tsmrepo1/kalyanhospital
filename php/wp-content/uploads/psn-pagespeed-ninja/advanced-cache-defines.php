66dc4cf09ea6b42f8285be46eae352dbf272f3b6
unknown <it@thinksurfmedia.com>
2023-12-19 11:52:47 +0530

    8 commit

---
+++ php/wp-content/uploads/psn-pagespeed-ninja/advanced-cache-defines.php
@@ -0,0 +1,26 @@
+<?php
+/* PageSpeed Ninja Caching */
+defined('ABSPATH') || die();
+define('PAGESPEEDNINJA_CACHE_DIR', '/home/thinksur/kalyanhospital.thinksurfmedia.com/php/wp-content/uploads/psn-pagespeed-ninja/pagecache');
+define('PAGESPEEDNINJA_CACHE_PLUGIN', '/home/thinksur/kalyanhospital.thinksurfmedia.com/php/wp-content/plugins/psn-pagespeed-ninja');
+define('PAGESPEEDNINJA_CACHE_RESSDIR', '/home/thinksur/kalyanhospital.thinksurfmedia.com/php/wp-content/plugins/psn-pagespeed-ninja/ress');
+define('PAGESPEEDNINJA_CACHE_DEPS_WEBP', true);
+define('PAGESPEEDNINJA_CACHE_DEPS_AVIF', true);
+define('PAGESPEEDNINJA_CACHE_DEPS_VENDOR', true);
+define('PAGESPEEDNINJA_CACHE_TTL', 604800);
+define('PAGESPEEDNINJA_CACHE_DISABLE_QUERIES', false);
+define('PAGESPEEDNINJA_CACHE_PARAMS_SKIP', 'utm_source
+utm_medium
+utm_campaign
+utm_content
+utm_term
+gclid
+fbclid
+dclid
+yclid');
+define('PAGESPEEDNINJA_CACHE_COOKIES_DISABLE', 'woocommerce_items_in_cart
+wordpress_logged_in_8781a8d1be4f0aee9343e811c72e87d2
+comment_author_8781a8d1be4f0aee9343e811c72e87d2');
+define('PAGESPEEDNINJA_CACHE_COOKIES_DEPEND', 'wp_lang');
+define('PAGESPEEDNINJA_CACHE_EXCLUDE_URLS', '/cart
+/checkout');
